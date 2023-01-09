dd6984cc1c0310cb77ca24560c7b2aa65f92ff05
Long Bui <Long.Bui@willistowerswatson.com>
2023-01-09 19:56:28 +0000

    13 model relationships

--- eTickets/Program.cs
+++ eTickets/Program.cs
@@ -1,5 +1,8 @@
-var builder = WebApplication.CreateBuilder(args);
+using eTickets.Data;
 
+var builder = WebApplication.CreateBuilder(args);
+//DbContext Configuration
+builder.Services.AddDbContext<AppDbContext>();
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
